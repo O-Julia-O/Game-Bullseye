9acd309069606f78c74f2af720501b97ed395db8
O-Julia-O <julia.qq.onix@gmail.com>
2023-06-02 21:18:52 +0300

    Periodically adding new eggs

--- script.js
+++ script.js
@@ -148,16 +148,18 @@ window.addEventListener('load', function() {
     class Egg {
         constructor(game) {
             this.game = game;
-            this.collisionX = Math.random() * this.game.width;
-            this.collisionY = Math.random() * this.game.height;
             this.collisionRadius = 40;
+            this.margin = this.collisionRadius * 2;
+            this.collisionX = this.margin + (Math.random() * this.game.width - this.margin * 2);
+            this.collisionY = this.game.topMargin + (Math.random() * (this.game.height - this.game.topMargin - this.margin));
+           
             this.image = document.getElementById('egg');
             this.spriteWidth = 110;
             this.spriteHeight = 135;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
-            this.spriteX = this.collisionX + this.width * 0.5;
-            this.spriteY = this.collisionY + this.height * 0.5;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5 - 30;
         }
 
         draw(context) {
@@ -172,6 +174,10 @@ window.addEventListener('load', function() {
                 context.stroke();
             }
         }
+
+        update() {
+            
+        }
     }
 
     class Game {
@@ -185,6 +191,8 @@ window.addEventListener('load', function() {
             this.fps = 70;
             this.timer = 0;
             this.interval = 1000 / this.fps;
+            this.eggTimer = 0;
+            this.eggInterval = 500;
             this.numberOfObstacles = 5;
             this.maxEggs = 10; 
             this.obstacles = [];
@@ -228,6 +236,7 @@ window.addEventListener('load', function() {
             if (this.timer > this.interval) {
                 context.clearRect(0, 0, this.width, this.height);
                 this.obstacles.forEach(obstacle => obstacle.draw(context));
+                this.eggs.forEach(egg => egg.draw(context));
                 this.player.draw(context);
                 this.player.update();
                 this.timer = 0;
@@ -235,6 +244,14 @@ window.addEventListener('load', function() {
             }
 
             this.timer += deltaTime;
+
+            //add eggs periodically 
+            if (this.eggTimer > this.eggInterval && this.eggs.length < this.maxEggs) {
+                this.addEgg();
+                this.eggTimer = 0;
+            } else {
+                this.eggTimer += deltaTime;
+            }
         }
 
         checkCollision(a, b) {
@@ -247,7 +264,7 @@ window.addEventListener('load', function() {
         }
 
         addEgg() {
-            
+            this.eggs.push(new Egg(this));
         }
 
         init() {
