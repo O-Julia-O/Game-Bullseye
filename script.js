62719f395ee66eba262e308a288a6c690ad601ab
O-Julia-O <julia.qq.onix@gmail.com>
2023-06-02 20:24:39 +0300

    fps

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.addEventListener('load', function() {
             this.dy = 0;
             this.speedModifier = 3;
             this.spriteWidth = 255;
-            this.spriteHeight = 255;
+            this.spriteHeight = 256;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
             this.spriteX;
@@ -155,6 +155,9 @@ window.addEventListener('load', function() {
             this.topMargin = 260;
             this.debug = true;
             this.player = new Player(this);
+            this.fps = 70;
+            this.timer = 0;
+            this.interval = 1000 / this.fps;
             this.numberOfObstacles = 5;
             this.obstacles = [];
 
@@ -192,11 +195,17 @@ window.addEventListener('load', function() {
             });
         }
 
-        render(context) {
-            this.obstacles.forEach(obstacle => obstacle.draw(context));
-            this.player.draw(context);
-            this.player.update();
-            
+        render(context, deltaTime) {
+            if (this.timer > this.interval) {
+                context.clearRect(0, 0, this.width, this.height);
+                this.obstacles.forEach(obstacle => obstacle.draw(context));
+                this.player.draw(context);
+                this.player.update();
+                this.timer = 0;
+
+            }
+
+            this.timer += deltaTime;
         }
 
         checkCollision(a, b) {
@@ -243,12 +252,14 @@ window.addEventListener('load', function() {
     game.init();
     console.log(game)
     
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.render(ctx);
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        game.render(ctx, deltaTime);
         requestAnimationFrame(animate);
     }
 
-    animate();
+    animate(0);
 
 })
